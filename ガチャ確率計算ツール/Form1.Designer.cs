09dfad66363a4637e5b8ae931632bea6990a6c0c
YoshinoriKamimoto <k.yoshinori0210@gmail.com>
2023-01-06 18:06:21 +0900

    ガチャ確率計算機

---
+++ ガチャ確率計算ツール/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ガチャ確率計算ツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tryCntTextBox = new System.Windows.Forms.TextBox();
+            this.tryCntLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tryCntTextBox
+            // 
+            this.tryCntTextBox.Location = new System.Drawing.Point(94, 66);
+            this.tryCntTextBox.Name = "tryCntTextBox";
+            this.tryCntTextBox.Size = new System.Drawing.Size(75, 19);
+            this.tryCntTextBox.TabIndex = 0;
+            // 
+            // tryCntLabel
+            // 
+            this.tryCntLabel.AutoSize = true;
+            this.tryCntLabel.Location = new System.Drawing.Point(92, 51);
+            this.tryCntLabel.Name = "tryCntLabel";
+            this.tryCntLabel.Size = new System.Drawing.Size(53, 12);
+            this.tryCntLabel.TabIndex = 1;
+            this.tryCntLabel.Text = "試行回数";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(94, 110);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 3;
+            this.calcButton.Text = "計算";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "※排出率などは設定ファイルで変更してね。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 217);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.tryCntLabel);
+            this.Controls.Add(this.tryCntTextBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ガチャ確率計算ツール";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tryCntTextBox;
+        private System.Windows.Forms.Label tryCntLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ガチャ確率計算ツール/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ガチャ確率計算ツール
+{
+    public partial class Form1 : Form
+    {
+        // 排出率
+        private double emissionRate = 0.00;
+
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        /// <summary>
+        /// 計算ボタンClickイベント
+        /// </summary>
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            // 設定ファイルから設定値を取得
+            try
+            {
+                GetSettingsValue();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"設定ファイルの読み取り中にエラーが発生しました。\n{ex}",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return;
+            }
+
+
+            // 排出率をチェック
+            if (emissionRate == 0.00)
+            {
+                MessageBox.Show("設定ファイルに排出率が規定されていないか0%に設定されているため、\n計算を中止します。",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return;
+            }
+
+
+            // 試行回数をチェックして取得
+            if (tryCntTextBox.Text == null || tryCntTextBox.Text == "")
+            {
+                MessageBox.Show("試行回数を入力してください。",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return;
+            }
+
+            int tryTimes;
+            if (int.TryParse(tryCntTextBox.Text, out tryTimes) == false)
+            {
+                MessageBox.Show("試行回数は数値で入力してください。",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return;
+            }
+
+            if (tryTimes <= 0)
+            {
+                MessageBox.Show("試行回数は0より大きい数値で入力してください。",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return;
+            }
+
+
+            // ガチャの取得確率を計算
+            double result;
+            try
+            {
+                result = CalcGettingRatio(tryTimes);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"取得確率の計算中にエラーが発生しました。\n\n{ex}",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return;
+            }
+
+
+            // 結果を表示
+            MessageBox.Show($"{tryTimes}回引いて少なくとも1枚あたる確率は、\n{result}%です。",
+                            "結果",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+
+            tryCntTextBox.Clear();
+        }
+
+
+
+        /// <summary>
+        /// 設定ファイルから設定値を取得するメソッド
+        /// </summary>
+        private void GetSettingsValue()
+        {
+            string line;
+
+            try
+            {
+                // 3つ上の階層にあるSettingsフォルダ配下の設定ファイルパスを取得
+                string currentFolderPath = Directory.GetCurrentDirectory();
+                string settingsFilePath = GetParentFolderPath(currentFolderPath, 3) + @"\Settings\Settings.ini";
+
+                // 設定ファイルを読み込む
+                using (StreamReader sr = new StreamReader(settingsFilePath))
+                {
+                    // 全行読み込む
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        // 一行ずつ読み込み、対象の文字列があれば値を取得し、変数に代入
+                        if (line.Contains("EmissionRate") == true)
+                        {
+                            emissionRate = double.Parse(line.Substring(12).Replace("=", "").Replace(" ", ""));
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+
+        /// <summary>
+        /// n個上の階層パスを取得するメソッド
+        /// </summary>
+        /// <param name="path">基点となるパス</param>
+        /// <param name="n">何個上に行くかを指定</param>
+        private string GetParentFolderPath(string path, int n)
+        {
+            // n回ループ
+            for (int i = 0; i < n; i++)
+            {
+                path = path.Substring(0, path.LastIndexOf("\\"));
+            }
+
+            // インデックスを返却
+            return path;
+        }
+
+
+
+        /// <summary>
+        /// ガチャの取得確率を計算するメソッド
+        /// </summary>
+        private double CalcGettingRatio(int times)
+        {
+            // n回引いて1枚も取得できない確率を計算
+            // (1- 排出率)のn乗
+            double ratio = Math.Pow(1 - emissionRate, times);
+
+            // n回引いて少なくとも1枚は取得できる確率を計算
+            // (1 - 1枚も取得できない確率)
+            ratio = 1 - ratio;
+
+            // %用に数値を変換して値を返却
+            double per = Math.Round(ratio * 100, 1);
+            return per;
+        }
+    }
+
+}
